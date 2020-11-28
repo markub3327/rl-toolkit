e185637b25ca789ba3314dd1bd71211050d71a01
Martin <markub3327@gmail.com>
2020-11-28 10:30:44 +0100

    update

--- receiver.py
+++ receiver.py
@@ -7,6 +7,7 @@ sock_receiver.initalize_receiver(8888)
 
 while True:
     frame = sock_receiver.receive_array()
+    frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
 
     # Display
     cv2.imshow('frame', frame)
--- sender.py
+++ sender.py
@@ -5,9 +5,9 @@ from utils.npsocket import SocketNumpyArray
 cap = cv2.VideoCapture(0)
 sock_sender = SocketNumpyArray()
 
-sock_sender.initialize_sender('192.168.1.11', 8888)
+sock_sender.initialize_sender('192.168.1.7', 8888)
 
 while True:
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (620, 480))
-    sock_sender.send_numpy_array(frame)
+    frame = cv2.resize(frame, (640, 360))
+    sock_sender.send_numpy_array(frame[:,:, 0])
