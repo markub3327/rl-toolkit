5425b1e3fba1687e083edd5d2bd670aa56dfc173
Martin Kubovčík <markub3327@gmail.com>
2020-12-15 17:14:45 +0100

    'relu' activation

--- sac/network.py
+++ sac/network.py
@@ -13,14 +13,14 @@ class Actor:
     def __init__(self, state_shape=None, action_shape=None, lr=None, model_path=None):
         if model_path == None:
             state_input = Input(shape=state_shape, name='state_input')
-            l1 = Dense(400, activation='swish', name='h1')(state_input)
-            l2 = Dense(300, activation='swish', name='h2')(l1)
+            l1 = Dense(400, activation='relu', name='h1')(state_input)
+            l2 = Dense(300, activation='relu', name='h2')(l1)
         
             # vystupna vrstva   -- 'mean' musi byt v intervale (-∞, ∞), 'std' musi byt v intervale (0, ∞)
             mean = Dense(action_shape[0], activation='linear', name='mean')(l2)
             scale = Dense(action_shape[0], activation=None, name='log_std')(l2)
             scale = Lambda(lambda x: tf.math.softplus(x) + 1e-5, name='std')(scale)
-            
+
             # Vytvor model
             self.model = Model(inputs=state_input, outputs=[mean, scale])
         else:
@@ -71,8 +71,8 @@ class Critic:
             action_input = Input(shape=action_shape, name='action_input')
 
             merged = Concatenate()([state_input, action_input])
-            l1 = Dense(400, activation='swish', name='h1')(merged)
-            l2 = Dense(300, activation='swish', name='h2')(l1)
+            l1 = Dense(400, activation='relu', name='h1')(merged)
+            l2 = Dense(300, activation='relu', name='h2')(l1)
 
             # vystupna vrstva   -- Q hodnoty su v intervale (-∞, ∞)!!!
             output = Dense(1, activation='linear', name='q_val')(l2)
