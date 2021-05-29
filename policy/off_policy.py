d3ed59bffa21f31617c72403620196de9c42d07a
Martin <markub3327@gmail.com>
2021-05-29 20:58:56 +0200

    fix shape [1]

--- policy/off_policy.py
+++ policy/off_policy.py
@@ -71,12 +71,12 @@ class OffPolicy(ABC):
                             [*self._env.action_space.shape],
                             self._env.action_space.dtype,
                         ),
-                        "rew": tf.TensorSpec([], tf.float32),
+                        "rew": tf.TensorSpec([1], tf.float32),
                         "obs2": tf.TensorSpec(
                             [*self._env.observation_space.shape],
                             self._env.observation_space.dtype,
                         ),
-                        "done": tf.TensorSpec([], tf.float32),
+                        "done": tf.TensorSpec([1], tf.float32),
                     },
                 )
             ],
