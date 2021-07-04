748b4f2126fa4e17526cd7a8dc112e9352db1ca7
markub <markub3327@gmail.com>
2021-07-04 13:35:12 +0200

    + Adam

--- rl_toolkit/policy/learner.py
+++ rl_toolkit/policy/learner.py
@@ -3,7 +3,7 @@ import os
 import reverb
 import tensorflow as tf
 import wandb
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import plot_model
 
 from rl_toolkit.networks import ActorCritic
@@ -72,9 +72,9 @@ class Learner(Policy):
         )
         self.model.build((None,) + self._env.observation_space.shape)
         self.model.compile(
-            actor_optimizer=RMSprop(learning_rate=actor_learning_rate),
-            critic_optimizer=RMSprop(learning_rate=critic_learning_rate),
-            alpha_optimizer=RMSprop(learning_rate=alpha_learning_rate),
+            actor_optimizer=Adam(learning_rate=actor_learning_rate),
+            critic_optimizer=Adam(learning_rate=critic_learning_rate),
+            alpha_optimizer=Adam(learning_rate=alpha_learning_rate),
         )
 
         # Show models details
