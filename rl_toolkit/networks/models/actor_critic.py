875520f475b0aaed2594d90ecc4cbcc79ebc2666
Martin <markub3327@gmail.com>
2021-09-06 20:04:49 +0200

    fixed ImportError

--- rl_toolkit/networks/models/actor_critic.py
+++ rl_toolkit/networks/models/actor_critic.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import Model
 
-from rl_toolkit.networks.models import Actor, MultiCritic
+from .actor import Actor
+from .critic import MultiCritic
 
 
 class ActorCritic(Model):
