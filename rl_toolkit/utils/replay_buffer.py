3f77756bfc7dc41cec02123fc7c79a60fa2d9313
Martin <markub3327@gmail.com>
2021-12-25 18:23:39 +0100

    num_parallel_calls -AUTOTUNE

--- rl_toolkit/utils/replay_buffer.py
+++ rl_toolkit/utils/replay_buffer.py
@@ -1,15 +1,19 @@
+import os
 import reverb
 import tensorflow as tf
 
 
 def make_reverb_dataset(
-    server_address: str, table: str, batch_size: int, num_parallel_calls: int = 12
+    server_address: str, table: str, batch_size: int, num_parallel_calls: int = None
 ):
+    if num_parallel_calls is None:
+        num_parallel_calls = os.cpu_count()
+
     def _make_dataset(unused_idx):
         dataset = reverb.TrajectoryDataset.from_table_signature(
             server_address=server_address,
             table=table,
-            max_in_flight_samples_per_worker=2 * batch_size,
+            max_in_flight_samples_per_worker=(2 * batch_size),
         )
 
         dataset = dataset.batch(batch_size, drop_remainder=True)
