f4850da64b171d5304424d5d6dfd1475ca67892e
Martin Kubovcik <markub3327@gmail.com>
2022-09-18 07:44:10 +0200

    fix

--- rl_toolkit/networks/models/actor_critic.py
+++ rl_toolkit/networks/models/actor_critic.py
@@ -257,9 +257,6 @@ class ActorCritic(Model):
     def build(self, input_shape):
         super(ActorCritic, self).build(input_shape)
 
-        self.counter_target.build(input_shape)
-        self.critic_target.build(input_shape)
-
         self._update_target(self.counter, self.counter_target, tau=1.0)
         self._update_target(self.critic, self.critic_target, tau=1.0)
 
