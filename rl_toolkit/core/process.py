9ada3cbb698d3e5f34442f250ba5308e315cd482
Martin Kubovcik <markub3327@gmail.com>
2022-10-13 16:19:27 +0200

    + GPU dynamics mem

--- rl_toolkit/core/process.py
+++ rl_toolkit/core/process.py
@@ -1,6 +1,7 @@
 import gym
 import pybullet_envs  # noqa
 
+import tensorflow as tf
 
 class Process:
     """
@@ -20,6 +21,18 @@ class Process:
     ):
         # Herne prostredie
         self._env = gym.make(env_name, render_mode="human" if render else None)
+
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.list_logical_devices('GPU')
+                    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
 
     def run(self):
         pass
