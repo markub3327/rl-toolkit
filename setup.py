485458f8732eaee2aa5d1d8e2ee9fb736e4faaa7
Martin Kubovcik <markub3327@gmail.com>
2025-01-11 09:02:34 +0100

    update setup

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Robotics",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
